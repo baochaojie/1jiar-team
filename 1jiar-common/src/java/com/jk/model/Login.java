0f9a788400e7f904a8f544466f07a7d82385eae3
chencm1 <2315455921@qq.com>
2019-04-17 09:11:36 +0800

    登录

--- 1jiar-common/src/java/com/jk/model/Login.java
+++ 1jiar-common/src/java/com/jk/model/Login.java
@@ -1,4 +1,16 @@
 package com.jk.model;
 
+import lombok.Data;
+
+@Data
 public class Login {
+    private Integer id;
+
+    private String logon;
+
+    private String password;
+
+    private Integer dlrIdId;
+
+
 }
--- 1jiar-feign-user/src/main/java/com/jk/controller/UserController.java
+++ 1jiar-feign-user/src/main/java/com/jk/controller/UserController.java
@@ -1,4 +1,34 @@
 package com.jk.controller;
 
+import com.jk.model.Login;
+import com.jk.service.UserService;
+import org.apache.tomcat.websocket.AuthenticationException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@Controller
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping("ads")
+    @ResponseBody
+    public void asd(){
+        Login login = new Login();
+        login.setId(1);
+    }
+
+
+
+
+
+
 }
