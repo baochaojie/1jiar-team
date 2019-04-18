560e427ff9e72405519c67bcfd669d76b24012e7
lvgr <736211630@qq.com>
2019-04-18 23:23:52 +0800

    commit by guorui

--- 1jiar-common/src/java/com/jk/model/Owner.java
+++ 1jiar-common/src/java/com/jk/model/Owner.java
@@ -18,8 +18,11 @@ public class Owner {
     private String ownerSiteMenpai;
 
     private Integer ownerUserId;    //关联一张用户表
+    private String name;    //用户名字
+    private String phoneNum;    //用户手机号
 
     private String ownerSell;
 
-    private String ownerState;
+    private String ownerState="1";  //审核状态： 1是待审核，2是审核通过，3是未通过审核
+
 }
